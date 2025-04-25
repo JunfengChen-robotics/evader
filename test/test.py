bc2f10e1c26b5b4e49a2f50d47e1fa1955848568
JunfengChen-robotics <cjfbuaa@163.com>
2025-04-25 19:04:11 +0800

    modify usage documentation

--- test/test.py
+++ test/test.py
@@ -6,24 +6,21 @@ if __name__ == "__main__":
     
     
     # 初始化 evader
-    # evader = Evader(pos)
-    
-    # 设置初始状态
-    # evader.set_init_state()
-    # evader.update_env(pursuers)
+    # evader = Evader(pos=[0, 0])
     
     # 只处理一次环境信息
-    # evader.get_env_info()
+    # evader.get_env_info(case:str)
+    # evader.update_env(pursuers:List[Pursuer])
     
     
     # 每跑一次loop: 运行一次update env 以及 move一次
-    # evader.local_info_and_collision_check()
+    # evader.local_info_and_collision_check(pursuers) 
     # evader.update_env(pursuers)
     # evader.update_memory()
     # evader.select_goal()
-    # evader.sel_stgy(self.world)
+    # evader.sel_stgy()
     # evader.move()
-    
+
     
     pass
     
